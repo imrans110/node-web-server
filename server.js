b0afe2d9c41839f10d4ebd2e208b71ece191a42d
unknown <imrans110@hotmail.com>
2017-09-23 23:08:12 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+// res.render('maintainance.hbs');
+//
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('currentYear', () => {
+return new Date().getFullYear()
+});
+
+hbs.registerHelper('scream', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    code: 404,
+    title: "JSON File returned with error"
+  });
+});
+
+app.get('/', (req, res) =>{
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcome: 'Welcome user 251'
+  });
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('server is up and running');
+});
